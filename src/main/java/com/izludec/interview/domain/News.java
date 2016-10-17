e270151fb59108449179d8d7c8d9c0b886b542ba
unknown <izludec@hotmail.com>
2016-10-17 10:21:45 +0400

    make it better

--- src/main/java/com/izludec/interview/domain/News.java
+++ src/main/java/com/izludec/interview/domain/News.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.apache.log4j.Logger;
 
 @Entity
 @Table(name = "NEWS")
 public class News implements Serializable {
  
  private static final long serialVersionUID = -5527566248002296042L;
+ protected static Logger logger = Logger.getLogger("service");
   
  @Id
  @Column(name = "ID")
@@ -66,16 +68,19 @@ public class News implements Serializable {
  }
 
  public Date getDate() {
-     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
      try{
         return dateFormat.parse(this.date);
      }
-     catch(Exception e){}
-     return new Date();
+     catch(Exception e){
+     logger.debug(e.getMessage());
+     return null;
+     }
+     
  }
 
  public void setDate(Date date) {    
-     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
   this.date = dateFormat.format(new Date());
  }
 }
